323f0d914f7bdf26da58cea2777c2d1b0a2d1fe1
solid-rockets <af.dev@protonmail.com>
2024-06-15 18:27:34 +0900

    feat: rename and no dupes

--- insert.py
+++ insert.py
@@ -22,54 +22,71 @@
 import json
 import sys
 
-# Global variables
-input_wordlist = []
+# GLOBAL VARIABLES.
 cards = []
 must_read_old_json = False
+word_lines = []
 
-input_wordlist = ""
-input_json = ""
-output_json = ""
+input_wordlist_path = ""
+input_json_path = ""
+output_json_path = ""
 
+# HELPER FUNCTIONS.
+def checkCardIsDuplicate(list, front):
+    for card in list:
+        if card["front"] == front:
+            return True
+    return False
+
+# MAIN LOGIC.
 # Get filenames from args: input of fresh, input of old JSON, and output JSON
 # Case of no old JSON file
 if len(sys.argv) == 3:
-    input_wordlist = sys.argv[1]
-    output_json = sys.argv[2]
+    input_wordlist_path = sys.argv[1]
+    output_json_path = sys.argv[2]
     must_read_old_json = False
 
 # Case of old JSON file
 if len(sys.argv) == 4:
-    input_wordlist = sys.argv[1]
-    input_json = sys.argv[2]
-    output_json = sys.argv[3]
+    input_wordlist_path = sys.argv[1]
+    input_json_path = sys.argv[2]
+    output_json_path = sys.argv[3]
     must_read_old_json = True
 
 # Inform the user about what filenames need to be provided
 if len(sys.argv) < 3:
-    print("Usage: python insert.py <input_wordlist> <output_json>")
-    print("Usage: python insert.py <input_wordlist> <input_json> <output_json>")
+    print("Usage: python insert.py <input_wordlist_path> <output_json_path>")
+    print("Usage: python insert.py <input_wordlist_path> <input_json_path> <output_json_path>")
     exit()
 
 # Open the wordlist file
-with open(input_wordlist, "r") as file:
+with open(input_wordlist_path, "r") as file:
     word_lines = file.readlines()
 
 # Load old JSON file
 if must_read_old_json:
-  with open(input_json, "r") as file:
+  with open(input_json_path, "r") as file:
       old_json = json.load(file)
       cards = old_json["cards"]
 
 # Append the new flashcards to the list
 for line in word_lines:
     front, back = line.strip().split(":")
+    front = front.strip()
+    back = back.strip()
+
+    # Ignore duplicates
+    if checkCardIsDuplicate(cards, front):
+        print(f"Duplicate found: {front}")
+        continue
+
+    # All good.
     cards.append({
-        "front": front.rstrip(),
-        "back": back.lstrip(),
+        "front": front,
+        "back": back,
         "score": 0
     })
 
 # Output the result into a JSON file
-with open(output_json, "w") as file:
+with open(output_json_path, "w") as file:
     json.dump({"cards": cards}, file)
