621d0b1c44272d8e7fdcf2981a0e33dc66829146
solid-rockets <af.dev@protonmail.com>
2024-06-25 18:58:27 +0900

    feat: script for three automated tests

---
+++ three_tests.sh
@@ -0,0 +1,16 @@
+# Remember to setup the KIOKU_PATH environment variable to point to the root of the project.
+# Script must obtain name of the file that contains lines.
+if [ -z "$KIOKU_PATH" ]; then
+    echo "KIOKU_PATH is not set. Please set it to the root of the project."
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "Please provide the name of the file that contains lines. EXCLUDE THE .txt EXTENSION."
+    exit 1
+fi
+
+# Proceed with the pipeline.
+python3 $KIOKU_PATH/test.py $1.json --max=60 --correct=1
+python3 $KIOKU_PATH/test.py $1.json --max=30 --correct=2
+python3 $KIOKU_PATH/test.py $1.json --max=15 --correct=4
